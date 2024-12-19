bf9299c52cc2a8b661151fdf3da423d624cd1a55
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-12-19 10:45:41 +0100

    feat: basic extensions + base

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'astro/config'
+import expressiveCode from 'astro-expressive-code'
 
 export default defineConfig({
+  integrations: [
+    expressiveCode({
+      // TODO(HiDeoo)
+      themes: ['dracula'],
+    }),
+  ],
   site: 'https://astro-glob-loader-config-generator.netlify.app/',
 })
