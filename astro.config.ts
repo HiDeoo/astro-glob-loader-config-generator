19b62d6b9251b11d7af15728a9824066ec8a2949
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-12-19 19:57:45 +0100

    feat: preview ui

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,19 @@ import expressiveCode from 'astro-expressive-code'
 export default defineConfig({
   integrations: [
     expressiveCode({
-      // TODO(HiDeoo)
-      themes: ['dracula'],
+      themes: ['houston'],
+      styleOverrides: {
+        borderColor: 'var(--color-zinc-600)',
+        borderRadius: '0',
+        borderWidth: 'var(--size-px)',
+        codeBackground: 'var(--color-zinc-800)',
+        frames: {
+          editorActiveTabBackground: 'var(--color-zinc-800)',
+          editorActiveTabIndicatorTopColor: 'var(--color-purple-500)',
+          editorTabBarBackground: 'var(--color-zinc-700)',
+          frameBoxShadowCssValue: 'none',
+        },
+      },
     }),
   ],
   site: 'https://astro-glob-loader-config-generator.netlify.app/',
